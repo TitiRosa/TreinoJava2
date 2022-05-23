b67995c7fd3b7065869be5e80295d78316b92a1b
LeticiaMDaRosa <leticia.rosa@southsystem.com.br>
2022-05-22 21:55:01 -0300

    inclusão projeto

---
+++ src/main/java/service/ContractService.java
@@ -0,0 +1,34 @@
+package service;
+
+import entities.Contract;
+import entities.Installment;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class ContractService {
+
+    private OnlinePaymentService onlinePaymentService;
+
+    public ContractService(OnlinePaymentService onlinePaymentService) {
+        this.onlinePaymentService = onlinePaymentService;
+    }
+
+    public void processContract(Contract contract, int months){
+        double basicQuota = contract.getTotalValue() / months;
+        for (int i = 1; i <= months; i++){
+           double updateQuota = basicQuota + onlinePaymentService.interest(basicQuota, i);
+           //updateQuota = 202
+            double fullQuota = updateQuota + onlinePaymentService.paymentFee(updateQuota);
+            // fullQuota = 206.04
+            Date dueDate = addMonths(contract.getDate(), i);
+            contract.getInstallments().add(new Installment(dueDate, fullQuota));
+        }
+    }
+    private Date addMonths(Date date, int N){
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.MONTH, N);
+        return calendar.getTime();
+    }
+}
